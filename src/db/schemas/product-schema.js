b88fbb5910b690e296cfea62ea7fcb5eed05a5a7
kim hyuna <kimakuma808@gmail.com>
2022-10-31 12:26:35 +0000

    Add new file

---
+++ src/db/schemas/product-schema.js
@@ -0,0 +1,32 @@
+import { Schema } from "mongoose";
+
+const ProductSchema = new Schema(
+  {
+    CATEGORY: {
+      type: String,
+      required: true,
+    },
+    PRODUCT_NAME: {
+      type: String,
+      required: true,
+    },
+    TASTE: {
+      type: String,
+      required: true,
+    },
+    SIZE: {
+      type: String,
+      required: false,
+    },
+    PRICE: {
+      type: Number,
+      required: false,
+    }
+  },
+  {
+    collection: "product",
+    timestamps: true,
+  }
+);
+
+export { UserSchema };
