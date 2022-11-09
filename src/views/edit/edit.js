77e2c05f1188a7f90c5eaecb07ab9f6b68621915
zero86 <katanazero86@gmail.com>
2022-11-09 21:57:21 +0900

    [fix] 오늘은 지현님 코딩과외

--- src/views/edit/edit.js
+++ src/views/edit/edit.js
@@ -4,102 +4,84 @@ const $email = document.querySelector("#email");
 const $name = document.querySelector(".name");
 const $phoneNumber = document.querySelector(".phone-number");
 const $address = document.querySelector(".address");
-
-const $editBtns = document.querySelectorAll(".edit-btn");
+const $moreInformationForm = document.querySelector('#more-information-form');
 
 // DB 가져오기
 const email = sessionStorage.getItem("email");
 
 const getUsers = async (email) => {
-  const userList = await Api.get("/api/userlist");
-  return userList.find((user) => user.EMAIL === email);
+    const userList = await Api.get("/api/userlist");
+    return userList.find((user) => user.EMAIL === email);
 };
 
 const userInfo = await getUsers(email);
+const setUserInfo = (targetUserInfo) => {
+    // console.log(userInfo._id);
+    // 이름 없으면 회원님....
 
-// console.log(userInfo._id);
-// 이름 없으면 회원님....
+    const name = targetUserInfo.FULL_NAME ?? '';
+    const phoneNumber = targetUserInfo.PHONE_NUMBER ? targetUserInfo.PHONE_NUMBER : "입력 없음";
+    const address = targetUserInfo.ZIP_CODE
+        ? targetUserInfo.ZIP_CODE + targetUserInfo.ADDRESS1 + targetUserInfo.ADDRESS2
+        : "입력 없음";
 
-const name = "ㅇㅇㅇ";
-// const name = userInfo.FULL_NAME ? userInfo.FULL_NAME : "입력 없음";
-const phoneNumber = userInfo.PHONE_NUMBER ? userInfo.PHONE_NUMBER : "입력 없음";
-const address = userInfo.ZIP_CODE
-  ? userInfo.ZIP_CODE + userInfo.ADDRESS1 + userInfo.ADDRESS2
-  : "입력 없음";
+    // const zipC
 
-// const zipC
+    $email.innerText = email;
+    $name.innerText = name;
+    $phoneNumber.innerText = phoneNumber;
+    $address.innerText = address;
+};
+setUserInfo(userInfo);
 
-$email.innerText = email;
-$name.innerText = name;
-$phoneNumber.innerText = phoneNumber;
-$address.innerText = address;
 
-$editBtns.forEach((editButton) => {
-  editButton.addEventListener("click", (e) => {
+$moreInformationForm.addEventListener('click', function (e) {
+
+    // 추가정보 - 수정하기
+    if (e.target.classList.contains('edit-btn')) {
+        const targetEl = e.target;
+        const inputEl = targetEl.parentElement.querySelector('input');
+        inputEl.style.display = 'block';
+        const labelEl = targetEl.parentElement.querySelector('label');
+        labelEl.style.display = 'none';
+        const containerEl = targetEl.parentElement.parentElement.querySelector('.button-container');
+        containerEl.style.display = 'block';
+        targetEl.style.display = 'none';
+    }
+
+    // 추가정보 - 취소
+    if (e.target.classList.contains('cancel-button')) {
+        const targetEl = e.target;
+        const inputEl = targetEl.parentElement.parentElement.querySelector('.input-field > input');
+        inputEl.style.display = 'none';
+        const labelEl = targetEl.parentElement.parentElement.querySelector('.input-field > label');
+        labelEl.style.display = 'block';
+        const containerEl = targetEl.parentElement;
+        containerEl.style.display = 'none';
+        const editBtnEl = targetEl.parentElement.parentElement.querySelector('.input-field > .edit-btn');
+        editBtnEl.style.display = 'block';
+    }
+});
+
+$moreInformationForm.addEventListener('submit', async function (e) {
     e.preventDefault();
+    const targetUserId = userInfo._id;
 
-    const $form = e.target.closest("form");
-    const $label = $form.querySelector("label");
-    const $content = $label.innerText;
-    const $input = $form.querySelector('input[type="text"]');
-    $input.value = $content;
+    const params = {
+        FULL_NAME: e.target.FULL_NAME.value.trim() === '' ? null : e.target.FULL_NAME.value.trim(),
+        PHONE_NUMBER: e.target.PHONE_NUMBER.value.trim() === '' ? null : e.target.PHONE_NUMBER.value.trim(),
+    };
 
-    const $buttonContainer = $form.querySelector(".button-container");
-    const $edit = $form.querySelector(".edit-btn");
-    $buttonContainer.style.display = "block";
-    $edit.style.display = "none";
+    try {
+        const result = await Api.patch(`/api/users/${targetUserId}`, params);
+        e.target.parentElement.querySelector('.cancel-button').click();
+        setUserInfo(result);
 
-    $label.style.display = "none";
-    $input.style.display = "block";
-
-    // 취소 버튼 누른 경우
-    const $cancelButton = $form.querySelector(".cancel-button");
-    $cancelButton.addEventListener("click", (e) => {
-      e.preventDefault();
-
-      $buttonContainer.style.display = "none";
-      $edit.style.display = "block";
-
-      $label.style.display = "block";
-      $input.style.display = "none";
-      console.log("c");
-    });
-
-    // 확인 버튼 누른 경우
-    const $submitButton = $form.querySelector(".submit-button");
-    $submitButton.addEventListener("click", async (e) => {
-      e.preventDefault();
-
-      const id = userInfo._id;
-      console.log(id);
-      console.log($input.querySelector());
-      console.log($input.value);
-
-      const data = {
-        FULL_NAME: name,
-        PHONE_NUMBER: $input.value,
-        // target id에 따라 key 값 변경
-      };
-
-      console.log(data);
-
-      try {
-        await Api.patch(`/users/${id}`, data);
-        alert("dddd");
-      } catch (err) {
-        console.error(err.stack);
+    } catch (err) {
+        console.error(err);
         alert(
-          `문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`
+            `문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`
         );
-      }
+    }
 
-      $buttonContainer.style.display = "none";
-      $edit.style.display = "block";
-
-      $label.style.display = "block";
-      $input.style.display = "none";
-
-      console.log("s");
-    });
-  });
 });
