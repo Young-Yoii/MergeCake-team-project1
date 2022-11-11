5ae91dd09a2e9aafec7895ca773500717e0339e4
hyun <kimakuma808@gmail.com>
2022-11-11 23:40:28 +0900

    fix: order로 데이터 전송안되던 이슈 해결!

--- src/routers/main-router.js
+++ src/routers/main-router.js
@@ -6,7 +6,7 @@ import {adminRouter} from "./admin-router";
 const mainRouter = Router();
 
 // 1. 주문 추가
-mainRouter.post("/product", async (req, res, next) => {
+mainRouter.post("/product/:userEmail", async (req, res, next) => {
   try {
     // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
     // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
@@ -16,6 +16,7 @@ mainRouter.post("/product", async (req, res, next) => {
         "headers의 Content-Type을 application/json으로 설정해주세요"
       );
     } */
+    const userEmail = req.params.userEmail;
 
     const maxOrderNo = await orderService.findMaxOrderNo();
 
@@ -25,7 +26,7 @@ mainRouter.post("/product", async (req, res, next) => {
 
     const price = newOrder.createdNewSelect.OPTIONS.price;
 
-    const newShipping = await orderService.addShipping(maxOrderNo, price);
+    const newShipping = await orderService.addShipping(userEmail, maxOrderNo, price);
 
     // 추가된 데이터를 프론트에 다시 보내줌
     res.status(201).json(newShipping);
